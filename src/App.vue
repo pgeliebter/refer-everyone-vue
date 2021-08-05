a63512987fe3ab9480637c75fac8b4a8d29f1ec0
Pinny Geliebter <pgibralter@gmail.com>
2021-08-04 21:55:17 -0400

    adds login/logout/signup views

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,12 @@
       <router-link to="/">Home</router-link>
       |
       <router-link to="/about">About</router-link>
+      |
+      <router-link to="/login">login</router-link>
+      |
+      <router-link to="/logout">logout</router-link>
+      |
+      <router-link to="/signup">signup</router-link>
     </div>
     <router-view />
   </div>
