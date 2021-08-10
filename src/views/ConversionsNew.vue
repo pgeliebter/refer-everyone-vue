e751752c7a57285252fe8ee254f4b431c7aefcbe
Pinny Geliebter <pgibralter@gmail.com>
2021-08-10 15:11:33 -0400

    adds v-if in case campaign does not exist

--- src/views/ConversionsNew.vue
+++ src/views/ConversionsNew.vue
@@ -1,31 +1,34 @@
 /* this page's path is "/:campaignId/new/" */
 <template>
   <div class="conversions-new">
-    <h2>{{ conversion.campaign.name }}</h2>
-    <h2>{{ conversion.campaign.company }}</h2>
-    <p>{{ newConversionParams }}</p>
-    <p>{{ messages.noMatch }}</p>
-    <form v-on:submit.prevent="newConversion()">
-      <div>
-        First Name:
-        <input type="text" v-model="newConversionParams.first_name" />
-      </div>
-      <div>
-        Last Name:
-        <input type="text" v-model="newConversionParams.last_name" />
-      </div>
-      <div>
-        Email:
-        <input type="text" v-model="newConversionParams.email" />
-      </div>
-      <div>
-        Phone:
-        <input type="text" v-model="newConversionParams.phone" />
-      </div>
-      <div>
-        <input type="submit" />
-      </div>
-    </form>
+    <div v-if="match">
+      <h2>{{ campaign.name }}</h2>
+      <h2>{{ campaign.company }}</h2>
+      <p>{{ newConversionParams }}</p>
+
+      <form v-on:submit.prevent="newConversion()">
+        <div>
+          First Name:
+          <input type="text" v-model="newConversionParams.first_name" />
+        </div>
+        <div>
+          Last Name:
+          <input type="text" v-model="newConversionParams.last_name" />
+        </div>
+        <div>
+          Email:
+          <input type="text" v-model="newConversionParams.email" />
+        </div>
+        <div>
+          Phone:
+          <input type="text" v-model="newConversionParams.phone" />
+        </div>
+        <div>
+          <input type="submit" />
+        </div>
+      </form>
+    </div>
+    <div v-else>hllo</div>
   </div>
 </template>
 <script>
@@ -35,8 +38,8 @@ export default {
   data: function () {
     return {
       newConversionParams: { campaign_id: this.$route.params.campaignId, referred_by: this.$route.query.referredBy },
-      messages: { match: true },
-      conversion: {},
+      match: false,
+      campaign: {},
     };
   },
   watch: {
@@ -44,24 +47,22 @@ export default {
       console.log(this.newConversionParams);
     },
   },
-  created: function () {
-    axios.get(`/conversions/${this.$route.params.campaignId}`).then((response) => {
-      this.conversion = response.data;
-      console.log(this.conversion);
-    });
-  },
   mounted: function () {
     this.validateCampaign();
   },
   methods: {
     // the below method validates the campaign from the route and make sure it exists
     validateCampaign: function () {
-      axios.get(`/campaigns/${this.$route.params.campaignId}`).catch((errors) => {
-        console.log(errors.response.status);
-        if (errors.response.status != 200) {
-          this.messages.match = true;
-        }
-      });
+      axios
+        .get(`/campaign/${this.$route.params.campaignId}`)
+        .then((response) => {
+          console.log(response.data);
+          this.campaign = response.data.campaign;
+          this.match = true;
+        })
+        .catch((errors) => {
+          console.log(errors.response.status);
+        });
     },
 
     newConversion: function () {
