5a4c13fed159aee9b6b6a7af1691db587d635947
Pinny Geliebter <pgibralter@gmail.com>
2021-08-30 10:16:25 -0400

    adds config for Heroku backend

--- src/main.js
+++ src/main.js
@@ -6,7 +6,8 @@ import Vue2Filters from "vue2-filters";
 
 Vue.use(Vue2Filters);
 
-axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
+axios.defaults.baseURL =
+  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://refer-everyone-rails.herokuapp.com";
 
 var jwt = localStorage.getItem("jwt");
 if (jwt) {
